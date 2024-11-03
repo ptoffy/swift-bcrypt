0796cdc2794349bf7b266c76b224f58bb90299ce
Paul Toffoloni <paultoffoloni@gmail.com>
2024-11-04 00:01:04 +0100

    Add EksBlowfish setup

--- Tests/BcryptTests/BcryptTests.swift
+++ Tests/BcryptTests/BcryptTests.swift
@@ -1,8 +1,4 @@
+import Foundation
 import Testing
 
 @testable import Bcrypt
-
-@Test func base64Encode() async throws {
-    let b64Encoded = Base64.encode(Array("password".utf8))
-    #expect(b64Encoded == "cGFzc3dvcmQ=")
-}
